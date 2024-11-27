df1558c0866672a7c8754105e5b4b2e52675e9a5
Streamlet <streamlet@outlook.com>
2024-11-27 20:27:06 +0800

    update

--- add_and_sub.py
+++ add_and_sub.py
@@ -2,20 +2,25 @@ import sys
 import random
 
 
-def generate(sum_upper_bound):
-    for i in range(sum_upper_bound + 1):
-        for j in range(sum_upper_bound + 1 - i):
-            yield (i, j)
-
+def generate(nums, lower_bound, upper_bound):
+    for i in range(lower_bound, upper_bound + 1):
+        if nums - 1 <= 0:
+            yield (i,)
+        else:
+            for t in generate(nums - 1, lower_bound, upper_bound - i):
+                yield (i, *t)
 
 def main():
-    sum_upper_bound = 100
+    lower_bound = 0
+    upper_bound = 100
     if len(sys.argv) > 1:
-        sum_upper_bound = int(sys.argv[1])
+        lower_bound = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        upper_bound = int(sys.argv[2])
     expressions = []
-    for (i, j) in generate(sum_upper_bound):
-        expressions.append('%-2d+%2d =' % (i, j))
-        expressions.append('%-2d-%2d =' % (i + j, i))
+    for t in generate(2, lower_bound, upper_bound):
+        expressions.append('%-2d+%2d =' % (t[0], t[1]))
+        expressions.append('%-2d-%2d =' % (sum(t), t[0]))
     random.shuffle(expressions)
     for expression in expressions:
         print(expression)
